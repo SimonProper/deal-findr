ad9ed9f1ba48982057078949fe25f00d71057087
Simon Proper <62900484+SimonProper@users.noreply.github.com>
2023-11-02 19:54:41 +0100

    Installed puppeteer

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,18 @@
-console.log("Hello World world!");
+import puppeteer from "puppeteer";
+
+(async () => {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+
+  // Navigate the page to a URL
+  await page.goto("https://developer.chrome.com/");
+
+  // Set screen size
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  const content = await page.content();
+  console.log(content);
+
+  await browser.close();
+})();
