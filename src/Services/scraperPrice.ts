9b31868bf0a40b6053eaeb54ae2792d69774ab9b
rohret <rohret@hotmail.se>
2023-11-05 14:12:52 +0100

    Implemented another method, scraperPrice seems to work

---
+++ src/Services/scraperPrice.ts
@@ -0,0 +1,45 @@
+import puppeteer from "puppeteer";
+
+export async function scrapeWebPagePrice(
+  url: string,
+  ProductName: string
+): Promise<void> {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+
+  // Required due to security protections (bypass Cloudflare's bot detection) (nlyman)
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+  );
+
+  await page.goto(url);
+
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  // Find all <script> elements with type "application/ld+json"
+  const ldJsonScripts = await page.$$eval(
+    'script[type="application/ld+json"]',
+    (scripts) => {
+      return scripts.map((script) => JSON.parse(script.innerHTML));
+    }
+  );
+
+  for (const script of ldJsonScripts) {
+    if (Array.isArray(script.offers)) {
+      if (script.offers.length > 0 && script.offers[0].price) {
+        console.log(ProductName + " pris: " + script.offers[0].price + " kr");
+      }
+      // TODO: Tror att det är lugnt om det är en lista av offer att ta första. Kontrollera innan detta tas bort.
+      //   for (const offer of script.offers) {
+      //     if (offer.price) {
+      //       console.log("Priss: " + offer.price);
+      //     }
+      //   }
+    } else if (script.offers && script.offers.price) {
+      // If "offers" is an object, access the price directly
+      console.log(ProductName + " pris: " + script.offers.price + " kr");
+    }
+  }
+
+  await browser.close();
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import { scrapeWebPage } from "./Services/scraper.js";
+import { scrapeWebPagePrice } from "./Services/scraperPrice.js";
 
 const NLYMAN_JEANS =
   "https://nlyman.com/se/produkt/woodbird-leroy-thun-black-jeans_841459-3294/";
-scrapeWebPage(NLYMAN_JEANS, "nlyman.txt");
-
 const HM_JEANS = "https://www2.hm.com/sv_se/productpage.1199024001.html";
-scrapeWebPage(HM_JEANS, "hm.txt");
-
 const ELGIGANTEN_MONITOR =
   "https://www.elgiganten.se/product/datorer-kontor/skarmar-tillbehor/datorskarm/samsung-odyssey-g8-s34bg850s-34-valvd-oled-bildskarm-silver/562075?gclid=CjwKCAjw15eqBhBZEiwAbDomEnRqk0L0ydadQPe29_PB8YE0UFvZ87K1DDnEUl4H9pXIPVyO63slKBoCV54QAvD_BwE&gclsrc=aw.ds";
+const ZALANDO_SWEATER =
+  "https://www.mq.se/bondelid-liam-half-zip-sweater-navy/?gclid=Cj0KCQjw-pyqBhDmARIsAKd9XIOYT9LyEECzYG_caeud4xVho5zqhobyw2YvFFCWJVObWd1w-XHeeZQaAqIpEALw_wcB";
+
+// Skriver ut alla priser på sidan (ej användbar)
+scrapeWebPage(NLYMAN_JEANS, "nlyman.txt");
+scrapeWebPage(HM_JEANS, "hm.txt");
 scrapeWebPage(ELGIGANTEN_MONITOR, "elgiganten.txt");
+
+// Fungerar
+scrapeWebPagePrice(ELGIGANTEN_MONITOR, "ELGIGANTEN_MONITOR");
+scrapeWebPagePrice(HM_JEANS, "HM_JEANS");
+scrapeWebPagePrice(NLYMAN_JEANS, "NLYMAN_JEANS");
+scrapeWebPagePrice(ZALANDO_SWEATER, "ZALANDO_SWEATER");
