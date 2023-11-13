e3e9e8344af8d02f2fa1de91759570378379a5f9
Simon Proper <62900484+SimonProper@users.noreply.github.com>
2023-11-13 21:17:18 +0100

    build: added and implemented turborepo

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col h-full w-full justify-center items-center p-24 min-h-screen">
+      <h1 className="text-5xl">Welcome to Deal Findr</h1>
+    </main>
+  );
+}
