55df03b9f26ae8dc14aba93428056186ad948232
rohret <rohret@hotmail.se>
2023-12-02 21:55:48 +0100

    restructure and added a more specific search

--- apps/scraper/src/services/scraperPrice.ts
+++ apps/scraper/src/services/scraperPrice.ts
@@ -1,42 +1,95 @@
 import puppeteer from "puppeteer";
 import fs from "fs";
 
-export async function scrapeWebPagePrice(
-  url: string
-): Promise<{ name: string; price: number | null }> {
+export async function scrapeWebPagePrice(url: string): Promise<{ name: string; price: number | null }> {
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
 
-  // Required due to security protections (bypass Cloudflare's bot detection) (nlyman)
+  // Required configurations
+  await configurePage(page, url);
+
+
+  let result;
+
+  // Finding the price and the name of the product
+  // #1 method, works 90 percent of the time
+  result = await findPriceAndName(page);
+
+  if (!result.price) {
+    result = await findPriceAndNameBroad(page);
+    console.log(result)
+  }
+
+
+  await browser.close();
+  return result;
+}
+
+async function configurePage(page: puppeteer.Page, url: string) {
   await page.setUserAgent(
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
   );
-
   await page.goto(url);
-
   await page.setViewport({ width: 1080, height: 1024 });
 
   // Required to fetch all information on some sites (elgiganten)
   // Note: 2 seconds may be excessive (should be tested)
   await new Promise<void>((r) => setTimeout(r, 2000));
+}
 
-  // Find all <script> elements with type "application/ld+json"
-  // This works when the website use use JSON-LD to include structured data (works almost everywhere).
-  const ldJsonScripts = await page.$$eval(
-    'script[type="application/ld+json"]',
-    (scripts) => {
-      return scripts.map((script) => JSON.parse(script.innerHTML));
+async function getJsonScripts(page: puppeteer.Page) {
+  return await page.$$eval('script[type="application/ld+json"]', (scripts) => {
+    return scripts.map((script) => JSON.parse(script.innerHTML));
+  });
+}
+
+async function searchWithSelectors(page: puppeteer.Page) {
+
+  // TODO: this should be a list in the DB
+  const AMAZON_NAME = '#productTitle';
+  const AMAZON_PRICE = 'span.a-price-whole';
+  const MELITTA_NAME = 'h1.product--title[itemprop="name"][data-qa-id="product-name"]';
+  const MELITTA_PRICE = 'span.price--content[data-qa-id="product-price"]';
+
+  const selectorsPrice = [AMAZON_PRICE, MELITTA_PRICE];
+  const selectorsName = [AMAZON_NAME, MELITTA_NAME];
+  let elementPrice = null;
+  let elementName = null;
+  let name = null;
+  let price = null;
+
+  for (const selectorPrice of selectorsPrice) {
+    elementPrice = await page.$(selectorPrice);
+    if (elementPrice) {
+      price = await page.evaluate(elementPrice => elementPrice.textContent, elementPrice);
+      break;
     }
-  );
+  }
+  for (const selectorName of selectorsName) {
+    elementName = await page.$(selectorName);
+    if (elementName) {
+      name = await page.evaluate(elementName => elementName.textContent, elementName);
+      break;
+    }
+  }
 
-  // Uncomment to save scraped content in files. 
-  // const content = await page.content();
-  // fs.writeFileSync("data/test_working.json", content);
-  // fs.writeFileSync("data/test.json", JSON.stringify(ldJsonScripts, null, 2));
+  console.log(price);
+  console.log(name);
 
+  return { name: name, price: price }
+}
 
+async function saveScrapedContent(page: puppeteer.Page, ldJsonScripts: any[]) {
+  const content = await page.content();
+  fs.writeFileSync("data/test_working.json", content);
+  fs.writeFileSync("data/test.json", JSON.stringify(ldJsonScripts, null, 2));
+}
 
-  // Finding the price and the name of the product
+async function findPriceAndName(page: puppeteer.Page) {
+  // Fetch structured data
+  const ldJsonScripts = await getJsonScripts(page);
+  await saveScrapedContent(page, ldJsonScripts);
+
   for (const script of ldJsonScripts) {
     let prodName = "";
     if (script['@type'] === 'Product' && script.name) {
@@ -45,22 +98,23 @@ export async function scrapeWebPagePrice(
     }
     if (Array.isArray(script.offers)) {
       if (script.offers.length > 0 && script.offers[0].price) {
-        console.log(
-          prodName + " pris: " + script.offers[0].price + " kr (no object)"
-        );
-        await browser.close();
+        console.log(prodName + " pris: " + script.offers[0].price + " kr (no object)");
         return { name: prodName, price: script.offers[0].price };
       }
     } else if (script.offers && script.offers.price) {
       // If "offers" is an object, access the price directly
-      console.log(
-        prodName + " pris: " + script.offers.price + " kr (object)"
-      );
-      await browser.close();
+      console.log(prodName + " pris: " + script.offers.price + " kr (object)");
       return { name: prodName, price: script.offers.price };
     }
   }
 
-  await browser.close();
   return { name: "", price: null };
+}
+
+async function findPriceAndNameBroad(page: puppeteer.Page) {
+  console.log("Method 2")
+
+  const productInfo = await searchWithSelectors(page);
+
+  return { name: productInfo.name, price: productInfo.price };
 }
