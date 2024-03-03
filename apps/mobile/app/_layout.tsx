866ce028e2edcaacf101d717cc2e3e11b74fec0c
Simon Proper <62900484+SimonProper@users.noreply.github.com>
2024-03-03 12:44:46 +0100

    fix(rootlayout): remove font loading

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -4,8 +4,7 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { Slot, SplashScreen, Stack, useRouter } from "expo-router";
+import { SplashScreen, Stack, useRouter } from "expo-router";
 import { useEffect } from "react";
 import { useColorScheme } from "react-native";
 import { useShareIntent } from "expo-share-intent";
@@ -16,24 +15,10 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const router = useRouter();
-  const [fontsLoaded, fontError] = useFonts({
-    ...FontAwesome.font,
-  });
+
   const { hasShareIntent, shareIntent, resetShareIntent } = useShareIntent({
     debug: true,
   });
-
-  useEffect(() => {
-    if (fontsLoaded || fontError) {
-      // Hide the splash screen after the fonts have loaded (or an error was returned) and the UI is ready.
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
-
-  // Prevent rendering until the font has loaded or an error was returned
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
 
   useEffect(() => {
     if (hasShareIntent) {
