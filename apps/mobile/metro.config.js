251d1f4103e6a335b4fa0141931a737b57ff0c9d
Simon Proper <62900484+SimonProper@users.noreply.github.com>
2024-03-03 14:12:18 +0100

    feat(mobile): add share to app (#3)
    * build: added base config for react native app
    
    * build(mobile): added expo router
    
    * build(mobile): added nativewind
    
    * fixed expo-router config for android
    
    * Added expo-dev-client & ios native dir
    
    * build(mobile): add share extenstion/intent
    
    * refactor: share intent config
    
    * refactor: use expo-share-intent library
    
    * fix(rootlayout): remove font loading
    
    * fix(rootlayout): remove manual splashscreen
    
    * refactor: move app to src
    
    * build(nativewind): upgrade to nativewind 4

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 const path = require("path");
 
 // Find the project and workspace directories
@@ -17,4 +18,4 @@ config.resolver.nodeModulesPaths = [
 // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
 config.resolver.disableHierarchicalLookup = true;
 
-module.exports = config;
+module.exports = withNativeWind(config, { input: "./src/styles/global.css" });
---
+++ apps/mobile/src/app/_layout.tsx
@@ -0,0 +1,44 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { SplashScreen, Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
+import { useColorScheme } from "react-native";
+import { useShareIntent } from "expo-share-intent";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import "../styles/global.css";
+
+export default function RootLayout() {
+  const router = useRouter();
+
+  const { hasShareIntent, shareIntent, resetShareIntent } = useShareIntent({
+    debug: true,
+  });
+
+  useEffect(() => {
+    if (hasShareIntent) {
+      router.replace({
+        pathname: "/share-intent",
+        params: { shareIntent: JSON.stringify(shareIntent) },
+      });
+      resetShareIntent();
+    }
+  }, [hasShareIntent]);
+
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <SafeAreaProvider>
+        <Stack />
+      </SafeAreaProvider>
+    </ThemeProvider>
+  );
+}
