d2a9db9a6d38d031d227478b00b60137cfd9e468
Simon Proper <62900484+SimonProper@users.noreply.github.com>
2024-09-25 21:35:12 +0200

    chore(web): delete old nextjs app

--- apps/web/src/trpc/utils.ts
+++
@@ -1,6 +0,0 @@
-// @filename: client.ts
-import type { AppRouter } from "@deal-findr/backend/src/trpc";
-import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
-
-export type RouterInput = inferRouterInputs<AppRouter>;
-export type RouterOutput = inferRouterOutputs<AppRouter>;
